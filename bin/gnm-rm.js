cf5d03abd3f8718d71de720f43c013fba8e7245f
treelite <c.xinle@gmail.com>
2016-01-11 17:44:50 +0800

    Delete all versions

--- bin/gnm-rm.js
+++ bin/gnm-rm.js
@@ -11,6 +11,7 @@ import * as meta from '../lib/meta';
 
 program
     .option('-f force', 'force delete module')
+    .option('-r roll', 'delete all versions module')
     .parse(process.argv);
 
 let [name, version] = (program.args[0] || '').split('@');
@@ -20,22 +21,26 @@ if (!name) {
     process.exit(1);
 }
 
-let fullName = name;
 let info = meta.get(name);
-if (version) {
-    fullName += '@' + version;
+
+if (!info) {
+    console.error(`${name} do not exist`);
+    process.exit(1);
 }
-else {
+
+if (version) {
+    program.roll = false;
+}
+else if (!program.roll) {
     version = info.versions[0];
 }
 
 if (info.ver && info.ver === version && !program.force) {
-    console.error(`can not remove ${fullName} currently in use`);
+    console.error(`can not remove ${name}@${version} currently in use`);
     process.exit(1);
 }
 
-rm(name, version)
-    .then(
-        info => console.log(`remove ${info.name}@${info.version}`),
-        error => console.error(error) || process.exit(1)
-    );
+let tasks = rm(name, version);
+Promise
+    .all(tasks.map(task => task.then(info => console.log(`remove ${info.name}@${info.version}`))))
+    .catch(error => console.error(error) || process.exit(1));
