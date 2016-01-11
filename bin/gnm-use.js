64991004d2eeaa8a66f2da5a030def46ad0b135b
treelite <c.xinle@gmail.com>
2016-01-11 18:55:23 +0800

    Default version is the highest

--- bin/gnm-use.js
+++ bin/gnm-use.js
@@ -17,9 +17,18 @@ program
 let [name, version] = (program.args[0] || '').split('@');
 
 if (!name) {
-    console.error('Please input package');
+    console.error('please input package name');
     process.exit(1);
 }
+
+let info = meta.get(name);
+
+if (!info) {
+    console.error(`${name} do not exist`);
+    process.exit(1);
+}
+
+version = version || info.versions[0];
 
 let before = Promise.resolve();
 if (!meta.has(name, version)) {
